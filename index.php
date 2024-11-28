1e63a6a7129d8836d442e6e8af05a8e5b823698f
Burnninel <brunoabc11@gmail.com>
2024-11-28 20:57:55 -0300

    create headear

--- index.php
+++ index.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ninel</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bokor&family=Luckiest+Guy&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <h1>Hello World!</h1>
+    <header class="bg-zinc-950 text-white p-4 tracking-wide">
+        <nav class="flex mx-auto max-w-screen-lg justify-between items-center
+                    sm:grid sm:grid-cols-4">
+            <ul class="text-xs text-slate-200 uppercase font-bold gap-3 sm:flex">
+                <li class="hover:underline cursor-pointer"><a href="#">Camisetas</a></li>
+                <li class="hover:underline cursor-pointer"><a href="#">Moletons</a></li>
+                <li class="hover:underline cursor-pointer"><a href="#">Calças</a></li>
+            </ul>
+            <div style="font-family: 'Bokor', cursive;"
+                class="text-center text-4xl uppercase text-emerald-900
+                       col-span-2 col-start-2 hover:animate-pulse cursor-pointer">
+                <a href="#">shopninel</a>
+            </div>
+        </nav>
+    </header>
 </body>
+
 </html>
