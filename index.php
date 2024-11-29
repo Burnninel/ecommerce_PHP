6b604ae1b1c1da9f71859bda2b04bbb2b721b067
Burnninel <brunoabc11@gmail.com>
2024-11-29 18:47:44 -0300

    Changing the Header Layout

--- index.php
+++ index.php
@@ -12,19 +12,27 @@
 </head>
 
 <body>
-    <header class="bg-zinc-950 text-white p-4 tracking-wide">
-        <nav class="flex mx-auto max-w-screen-lg justify-between items-center
-                    sm:grid sm:grid-cols-4">
-            <ul class="text-xs text-slate-200 uppercase font-bold gap-3 sm:flex">
-                <li class="hover:underline cursor-pointer"><a href="#">Camisetas</a></li>
-                <li class="hover:underline cursor-pointer"><a href="#">Moletons</a></li>
-                <li class="hover:underline cursor-pointer"><a href="#">Calças</a></li>
-            </ul>
+    <header class="tracking-wide">
+        <nav class="bg-zinc-900 text-center">
             <div style="font-family: 'Bokor', cursive;"
-                class="text-center text-4xl uppercase text-emerald-900
-                       col-span-2 col-start-2 hover:animate-pulse cursor-pointer">
+                class="text-5xl uppercase text-emerald-900 py-4
+                       hover:animate-pulse cursor-pointer">
                 <a href="#">shopninel</a>
             </div>
+            <ul class="sm:text-sm text-[0.6rem] text-gray-950 bg-zinc-50 uppercase font-bold flex">
+                <li class="cursor-pointer py-2 w-1/4
+                           hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
+                <a href="#">Camisetas</a></li>
+                <li class="cursor-pointer py-2 w-1/4
+                           hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
+                <a href="#">Moletons</a></li>
+                <li class="cursor-pointer py-2 w-1/4
+                           hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
+                <a href="#">Calças</a></li>
+                <li class="cursor-pointer py-2 w-1/4
+                           hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
+                <a href="#">Bermudas</a></li>
+            </ul>
         </nav>
     </header>
 </body>
