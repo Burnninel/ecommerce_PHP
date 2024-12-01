c29c851467f727ad742ce7b1e83271b951e4b9f6
Burnninel <brunoabc11@gmail.com>
2024-12-01 01:03:52 -0300

    Creating the first element (carousel) of the main

--- index.php
+++ index.php
@@ -4,17 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ninel</title>
+    <title>Shopninel</title>
+
     <script src="https://cdn.tailwindcss.com"></script>
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bokor&family=Luckiest+Guy&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="/swiper.css">
 </head>
 
 <body>
     <header class="tracking-wide">
         <nav class="bg-zinc-900 text-center">
-            <div style="font-family: 'Bokor', cursive;"
+            <div id="logo"
                 class="text-5xl uppercase text-emerald-900 py-4
                        hover:animate-pulse cursor-pointer">
                 <a href="#">shopninel</a>
@@ -22,19 +27,43 @@
             <ul class="sm:text-sm text-[0.6rem] text-gray-950 bg-zinc-50 uppercase font-bold flex">
                 <li class="cursor-pointer py-2 w-1/4
                            hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
-                <a href="#">Camisetas</a></li>
+                    <a href="#">Camisetas</a>
+                </li>
                 <li class="cursor-pointer py-2 w-1/4
                            hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
-                <a href="#">Moletons</a></li>
+                    <a href="#">Moletons</a>
+                </li>
                 <li class="cursor-pointer py-2 w-1/4
                            hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
-                <a href="#">Calças</a></li>
+                    <a href="#">Calças</a>
+                </li>
                 <li class="cursor-pointer py-2 w-1/4
                            hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
-                <a href="#">Bermudas</a></li>
+                    <a href="#">Bermudas</a>
+                </li>
             </ul>
         </nav>
     </header>
+
+    <main>
+        <section class="swiper size-2/3 mt-6">
+            <div class="swiper-wrapper flex items-center h-full">
+                <div class="swiper-slide flex items-center text-center justify-center">
+                    <a href="#" class="w-full"><img src="./img/lancamentos.png" alt="Lançamentos 2025" class="w-full"></a>
+                </div>
+                <div class="swiper-slide flex items-center text-center justify-center">
+                    <a href="#" class="w-full"><img src="./img/high.png" alt="Produtos High" class="w-full"></a>
+                </div>
+            </div>
+
+            <div class="swiper-button-next text-gray-100"></div>
+            <div class="swiper-button-prev text-gray-100"></div>
+            <div class="swiper-pagination"></div>
+        </section>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="/swiper.js"></script>
 </body>
 
 </html>
