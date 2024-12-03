b03c9d092b8d07cd796f7da0d89d8372f46ddf93
Burnninel <brunoabc11@gmail.com>
2024-12-02 21:10:31 -0300

    including footer

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Shopninel</title>
 
     <script src="https://cdn.tailwindcss.com"></script>
-    
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="/swiper.css">
 </head>
@@ -124,7 +124,34 @@
         </section>
     </main>
 
-    <div class="mt-[300px]">teste</div>
+    <footer class="mt-[10vh] bg-gray-900 p-[2vh] text-gray-400 text-center space-y-[2vh]">
+            <div class="text-center text-[6vh] uppercase max-w-screen-lg mx-auto">
+                <a href="#" class="text-emerald-900" id="logo">shopninel</a>
+                <div class="text-center text-[1.3vh] font-normal">
+                    Desde 2001, a Shopninel inspira autenticidade e estilo.
+                    Mais que moda, criamos conexões e peças que convidam você a explorar o mundo com confiança.
+                    Aprendemos que viver intensamente faz toda a diferença.
+                    <br> - Wear Your Story.
+                </div>
+            </div>
+
+            <ul class="font-semibold text-center">
+                <li class="text-center text-[3vh] uppercase text-emerald-900 mb-[1vh]">Fale conosco</li>
+                <li class="text-[1.3vh]">
+                    INSTAGRAM: <span class="text-gray-400 ml-2">@bruno_i79</span>
+                </li>
+                <li class="text-[1.3vh]">
+                    WHATSAPP: <span class="text-gray-400 ml-2">(11) 99999-9999</span>
+                </li>
+                <li class="text-[1.3vh]">
+                    EMAIL: <span class="text-gray-400 ml-2">contato@shopininel.com</span>
+                </li>
+            </ul>
+        <p class="text-center text-gray-400 text-[1.2vh] mt-4">
+            © SHOPNINEL | BRNO - TECNOLOGIA EM ECOMMERCE | CNPJ: 77779999/0001-99
+        </p>
+    </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script src="/swiper.js"></script>
 </body>
