986182e14a9acb488e1b03e81e5caf4ce5a1a9d1
Burnninel <brunoabc11@gmail.com>
2024-12-15 15:38:10 -0300

    Creating Product Database File

---
+++ dados.php
@@ -0,0 +1,32 @@
+<?php
+
+$composicao = [
+    'estampa' => 'Camiseta manga curta preta com silk colorido na frente, e preto e branco costas.',
+    'tecido' => 'Composição: 100% algodão, pré-encolhido com gramatura de 0,175g. que pode variar 3% para mais ou para menos.',
+    'medidas' => 'As medidas podem variar em 1cm para mais ou para menos em comparação com a grade de tamanhos.',
+    'obs' => '_Obs: A coloração dos produtos em fotos externas ou de campanha podem apresentar alterações. Na dúvida sobre a cor real do produto, veja a foto com fundo branco._'
+];
+
+$medidas = [
+    'peito' => ['p' => '53 cm', 'm' => '56 cm', 'g' => '59 cm', 'gg' => '62 cm', 'xgg' => '65 cm'],
+    'comprimento' => ['p' => '72 cm', 'm' => '74 cm', 'g' => '76 cm', 'gg' => '80 cm', 'xgg' => '84 cm'],
+    'manga' => ['p' => '22 cm', 'm' => '23 cm', 'g' => '24 cm', 'gg' => '25 cm', 'xgg' => '26 cm'],
+];
+
+$produtos = [
+    'id' => 00001,
+    'nome' => 'Camiseta Smoking Team Off White',
+    'preco' => 'R$189,00',
+    'descricao' => 'Você curte F1? ou F1? Ou curte F1 F1? Vrummmmmm',
+    'tags' => '#tchasco #pirraci #agitonanight Bolovo Since 2006©️ TMJ & Mist.',
+    'fabricacao' => 'FABRICADO NO BRASIL',
+    'medidas' => $medidas,
+    'composicao' => $composicao,
+    'lavagem' => 'Lavar na máquina com água fria. Secar no varal. Não usar alvejante. Não deixar de molho. Não colocar na secadora. Não lavar a seco. Passar do lado avesso em temperatura média.',
+    'tipo' => '',
+    'categoria' => 'bestSellers',
+    'imagems' => [
+        'imagem1' => 'bolovoFrente',
+        'imagem2' => 'bolovoCostas',
+    ],
+];
