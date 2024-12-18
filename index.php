0b39daf2ca899131c18f2ff950d4327dfe30216f
Burnninel <brunoabc11@gmail.com>
2024-12-18 19:51:28 -0300

    realizando verificação de qual imagem aparacer no hover de produtos

--- index.php
+++ index.php
@@ -87,8 +87,8 @@ require("dados.php")
                         <article class="swiper-slide flex w-[calc(25%)] justify-around cursor-pointer">
                             <a href="/produto.php?id=<?= $bestSellers['id'] ?>" class="w-full">
                                 <div class="relative h-[45vw] sm:h-[35vw] md:h-[30vw] lg:h-[25vw]">
-                                    <img src="/img/<?= $bestSellers['imagens']['frente'] ?>.png" alt="Imagem da frente" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 hover:opacity-0">
-                                    <img src="/img/<?= $bestSellers['imagens']['costas'] ?>.png" alt="Imagem de costas" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 opacity-0 hover:opacity-100">
+                                    <img src="/img/camisas/<?= $bestSellers['imagens']['frente'] ?>.png" alt="Imagem da frente" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 hover:opacity-0">
+                                    <img src="/img/camisas/<?= isset($bestSellers['imagens']['adicional2']) ? $bestSellers['imagens']['adicional2'] : $bestSellers['imagens']['adicional'] ?>.png" alt="Imagem de costas" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 opacity-0 hover:opacity-100">
                                 </div>
                                 <div class="flex flex-col sm:flex-row justify-between uppercase sm:text-[1.4vh] text-[1vh] text-zinc-800 py-[0.5vh]">
                                     <h2 class="font-bold"><?= $bestSellers['nome'] ?></h2>
@@ -119,11 +119,11 @@ require("dados.php")
             $produtosComBanner = array_slice($produtosComBanner, 0, 2);
             foreach ($produtosComBanner as $index => $produto): ?>
                 <div class="relative h-[50vw] w-1/2 cursor-pointer">
-                    <img src="/img/<?= ($index % 2 === 0) ? $produto['imagens']['vestidaFrente'] : $produto['imagens']['costas'] ?>.png"
+                    <img src="/img/camisas/<?= ($index % 2 === 0) ? $produto['imagens']['adicional'] : $produto['imagens']['costas'] ?>.png"
                         alt="Imagem da frente"
                         class="absolute inset-0 w-full h-full object-cover 
                         transition-opacity duration-500 hover:opacity-0">
-                    <img src="/img/<?= ($index % 2 === 0) ? $produto['imagens']['costas'] : $produto['imagens']['vestidaFrente'] ?>.png"
+                    <img src="/img/camisas/<?= ($index % 2 === 0) ? $produto['imagens']['costas'] : $produto['imagens']['adicional'] ?>.png"
                         alt="Imagem de costas"
                         class="absolute inset-0 w-full h-full object-cover 
                         transition-opacity duration-500 opacity-0 hover:opacity-100">
@@ -146,8 +146,8 @@ require("dados.php")
                         <li class="w-[calc(25%-1vh)] cursor-pointer">
                             <a href="/produto.php?id=<?= $lancamento['id'] ?>">
                                 <div class="relative h-[22vw]">
-                                    <img src="/img/<?= $lancamento['imagens']['frente'] ?>.png" alt="Imagem da frente" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 hover:opacity-0">
-                                    <img src="/img/<?= $lancamento['imagens']['costas'] ?>.png" alt="Imagem de costas" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 opacity-0 hover:opacity-100">
+                                    <img src="/img/camisas/<?= $lancamento['imagens']['frente'] ?>.png" alt="Imagem da frente" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 hover:opacity-0">
+                                    <img src="/img/camisas/<?= isset($lancamento['imagens']['adicional2']) ? $lancamento['imagens']['adicional2'] : $lancamento['imagens']['adicional'] ?>.png" alt="Imagem de costas" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 opacity-0 hover:opacity-100">
                                 </div>
                                 <div class="flex flex-col sm:flex-row justify-between uppercase sm:text-[1.4vh] text-[1vh] text-zinc-800 py-[0.5vh]">
                                     <h2 class="font-bold"><?= $lancamento['nome'] ?></h2>
