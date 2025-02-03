1d67bbc34597d570b89737d1fc8547334cad3094
Burnninel <brunoabc11@gmail.com>
2025-02-02 22:27:25 -0300

    Ajustando captura dinamica de imagens com php - part2
    Anteriormentente, no index busca as imagens direto na pasta 'camisetas', como agora temos mais tipos, ajustei para consultar dinamicamente conforme o tipo do produto. No primeiro commit faltou adicionar da ultima section.

--- views/index.view.php
+++ views/index.view.php
@@ -100,8 +100,8 @@
                 <li class="w-[calc(50%-1vh)] md:w-[calc(33.33%-1vh)] lg:w-[calc(25%-1vh)] cursor-pointer">
                     <a href="produto?id=<?= $lancamento['id'] ?>">
                         <div class="relative h-[45vw] sm:h-[45vw] md:h-[30vw] lg:h-[25vw]">
-                            <img src="/img/camisetas/<?= $lancamento['imagens']['frente'] ?>.png" alt="Imagem da frente" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 hover:opacity-0">
-                            <img src="/img/camisetas/<?= isset($lancamento['imagens']['adicional2']) ? $lancamento['imagens']['adicional2'] : $lancamento['imagens']['adicional'] ?>.png" alt="Imagem de costas" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 opacity-0 hover:opacity-100">
+                            <img src="/img/<?= $lancamento['tipo'] ?>/<?= $lancamento['imagens']['frente'] ?>.png" alt="Imagem da frente" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 hover:opacity-0">
+                            <img src="/img/<?= $lancamento['tipo'] ?>/<?= isset($lancamento['imagens']['adicional2']) ? $lancamento['imagens']['adicional2'] : $lancamento['imagens']['adicional'] ?>.png" alt="Imagem de costas" class="absolute inset-0 w-full h-full object-cover transition-opacity duration-500 opacity-0 hover:opacity-100">
                         </div>
                         <div class="flex flex-col sm:flex-row justify-between uppercase sm:text-[1.4vh] text-[1vh] text-zinc-800 py-[0.5vh]">
                             <h2 class="font-bold"><?= $lancamento['nome'] ?></h2>
