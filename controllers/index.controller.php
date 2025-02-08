b4fdd683b42e1f9a6d574cfd5dc80cc83c2200fa
Burnninel <brunoabc11@gmail.com>
2025-02-08 18:35:23 -0300

    Implementando a geração automatica de elementos do banner 'bestSellers'
    Anteriormente, o 'bannerBestsellers' capturava os itens de forma fixa. Com a nova implementação na controlloer, ocorre o filtro de produto bestSeller, captura no maximo 8 itens e retorna na view.

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -2,6 +2,16 @@
 
 require 'dados.php';
 
+$filtroBestSellers = array_filter($produtos, function($item) {
+    return isset($item['bestSeller']) && $item['bestSeller'] === true;
+});
+
+$contadorBestSellers  = min(count($filtroBestSellers), 8);
+
+$indicesBestSellers = array_rand($filtroBestSellers, $contadorBestSellers);
+$bestSeller = is_array($indicesBestSellers) ? array_intersect_key($filtroBestSellers, array_flip($indicesBestSellers)) : [$filtroBestSellers[$indices]];
+
 view('index', [
-    'produtos' => $produtos
+    'produtos' => $produtos,
+    'bestSeller' => $bestSeller
 ]);
