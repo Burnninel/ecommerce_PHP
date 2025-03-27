5a1655b78a63de37f31b337846d2a586f055630d
Burnninel <brunoabc11@gmail.com>
2025-03-27 19:11:04 -0300

    Ajustando a controller do index para consulta dinâmica
    A controller do index foi adaptada para utilizar uma consulta dinâmica ao banco de dados, substituindo as funções anteriores baseadas em arrays. As chamadas, SELECT e outras informações foram atualizadas para uma abordagem mais eficiente e centralizada, mantendo a lógica original.

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -1,6 +1,26 @@
 <?php
 
-$produto = (new DB())->produto();
+
+$produto = (new DB())->query(
+    query: "
+        SELECT 
+            p.id AS id, p.nome AS nome, p.preco AS preco,
+            p.banner AS banner, p.best_seller AS bestSeller, p.lancamento AS lancamento,  
+            e.categoria AS categoria,
+            (SELECT GROUP_CONCAT(caminho || '.' || tipo, ',') 
+            FROM imagens 
+            WHERE produto_id = p.id 
+            ORDER BY id) AS imagens
+        FROM produtos AS p
+        JOIN especificacao AS e ON e.id = p.especificacao_id
+        GROUP BY p.id
+    ",
+    class: $class = Produto::class,
+)->fetchAll();
+
+foreach ($produto as $item) {
+    $item->setImagens($item->imagens);
+}
 
 $bestSellers = array_filter($produto, fn($item) => $item->bestSeller === 1);
 $lancamentos = array_filter($produto, fn($item) => $item->lancamento === 1);
