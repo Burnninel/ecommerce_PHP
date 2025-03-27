b54f2e69f43fd867eb474d82c9f4051a9eb5927c
Burnninel <brunoabc11@gmail.com>
2025-03-27 19:14:52 -0300

    Ajustando a controller da categoria para consulta dinâmica
    A controller da categoria foi refatorada para utilizar consulta dinâmica ao banco de dados, substituindo as funções anteriores baseadas em arrays. As chamadas, SELECT e demais ajustes foram feitos para otimizar e centralizar o processo, mantendo a lógica original.

--- controllers/categoria.controller.php
+++ controllers/categoria.controller.php
@@ -3,7 +3,26 @@
 $categoria = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
 $filtroModelo = $_GET['modelo'] ?? 'todos';
 
-$produtos = (new DB())->produto();
+$produtos = (new DB())->query(
+    query: "
+        SELECT 
+            p.id AS id, p.nome AS nome, p.preco AS preco,
+            p.banner AS banner, p.best_seller AS bestSeller, p.lancamento AS lancamento,  
+            e.categoria AS categoria, e.modelo AS modelo,
+            (SELECT GROUP_CONCAT(caminho || '.' || tipo, ',') 
+            FROM imagens 
+            WHERE produto_id = p.id 
+            ORDER BY id) AS imagens
+        FROM produtos AS p
+        JOIN especificacao AS e ON e.id = p.especificacao_id
+        GROUP BY p.id
+    ",
+    class: $class = Produto::class,
+)->fetchAll();
+
+foreach ($produtos as $item) {
+    $item->setImagens($item->imagens);
+}
 
 $produtoCategoria = array_filter($produtos, function ($produto) use ($filtroModelo, $categoria) {
     return $produto->categoria === $categoria && 
