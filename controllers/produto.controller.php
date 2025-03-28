fbdd6298ec9aa94ac6685ed28ab4ef9a2e6a38f3
Burnninel <brunoabc11@gmail.com>
2025-03-27 21:06:07 -0300

    Refatoração do 'produto.controller' para consumo do service de produto
    A controller de produto foi refatorada para utilizar o service de produto, centralizando a lógica de consulta ao banco de dados. Com essa mudança, a controller agora chama o service, permitindo um código mais limpo, reutilizável e dinâmico, além de eliminar a repetição de código e melhorar a manutenção.

--- controllers/produto.controller.php
+++ controllers/produto.controller.php
@@ -2,34 +2,9 @@
 
 $id = $_REQUEST['id'];
 
-$listaProdutos = (new DB())->query(
-    query: "
-        SELECT 
-            p.id AS id, p.nome AS nome, p.preco AS preco, p.descricao AS descricao,
-            p.fabricacao as fabricacao, p.composicao AS composicao,
-            p.banner AS banner, p.best_seller AS bestSeller, p.lancamento AS lancamento,
-            e.categoria AS categoria, e.modelo AS modelo, 
-            m.nome AS marca, l.instrucoes AS lavagem,
-            (SELECT GROUP_CONCAT(caminho || '.' || tipo, ',') 
-            FROM imagens 
-            WHERE produto_id = p.id 
-            ORDER BY id) AS imagens
-        FROM produtos AS p
-        JOIN especificacao AS e ON e.id = p.especificacao_id
-        JOIN marcas AS m ON m.id = p.marca_id
-        JOIN lavagem AS l ON l.id = p.lavagem_id
-        WHERE p.id = :id
-        GROUP BY p.id
-    ",
-    class: $class = Produto::class,
-    params: $params = ['id' => "$id"]
-)->fetchAll();
-
-$produto = $listaProdutos[0] ?? null;
-
-foreach ($listaProdutos as $item) {
-    $item->setImagens($item->imagens);
-}
+$produtoService = new ProdutoServices();
+$produto = $produtoService->getProdutos($id)[0];
+$bestSellers = $produtoService->getProdutos(null, 'best_seller');
 
 $tabelaDeMedidas = (new DB())->query(
     query: "SELECT produto_id, tamanho, comprimento, peito, manga, cintura  FROM tabela_medidas WHERE produto_id = :id",
@@ -47,25 +22,6 @@ $medidas = array_unique(
         return $carry;
     })
 );
-
-$bestSellers = (new DB())->query(
-    query: "
-        SELECT p.id, p.nome, p.preco, e.categoria AS categoria, 
-            (SELECT GROUP_CONCAT(caminho || '.' || tipo, ',')  
-            FROM imagens 
-            WHERE produto_id = p.id 
-            ORDER BY id) AS imagens
-        FROM produtos AS p
-        JOIN especificacao AS e ON e.id = p.especificacao_id
-        WHERE p.best_seller = 1
-        GROUP BY p.id, p.nome, p.preco
-    ",
-    class: $class = Produto::class,
-)->fetchAll();
-
-foreach ($bestSellers as $item) {
-    $item->setImagens($item->imagens);
-}
 
 $dimensoes = array_filter($tabelaDeMedidas, fn($item) => $item !== null);
 
