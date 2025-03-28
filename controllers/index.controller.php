41d742694ee73a32336ac5a8279f525e0718ae77
Burnninel <brunoabc11@gmail.com>
2025-03-27 21:15:40 -0300

    Alterando variável de conexão ao banco para chave privada e configurando o construtor
    Neste commit, a variável de conexão ao banco de dados foi alterada para utilizar uma chave privada, garantindo maior segurança. Além disso, o construtor foi configurado para inicializar a conexão de forma mais eficiente, evitando a criação de novas instâncias de PDO desnecessárias.

--- controllers/index.controller.php
+++ controllers/index.controller.php
@@ -1,30 +1,10 @@
 <?php
 
+$produtoService = new ProdutoServices();
 
-$produto = (new DB())->query(
-    query: "
-        SELECT 
-            p.id AS id, p.nome AS nome, p.preco AS preco,
-            p.banner AS banner, p.best_seller AS bestSeller, p.lancamento AS lancamento,  
-            e.categoria AS categoria,
-            (SELECT GROUP_CONCAT(caminho || '.' || tipo, ',') 
-            FROM imagens 
-            WHERE produto_id = p.id 
-            ORDER BY id) AS imagens
-        FROM produtos AS p
-        JOIN especificacao AS e ON e.id = p.especificacao_id
-        GROUP BY p.id
-    ",
-    class: $class = Produto::class,
-)->fetchAll();
-
-foreach ($produto as $item) {
-    $item->setImagens($item->imagens);
-}
-
-$bestSellers = array_filter($produto, fn($item) => $item->bestSeller === 1);
-$lancamentos = array_filter($produto, fn($item) => $item->lancamento === 1);
-$banner = array_filter($produto, fn($item) => $item->banner === 1);
+$bestSellers = $produtoService->getProdutos(null, 'best_seller');
+$lancamentos = $produtoService->getProdutos(null, 'lancamento');
+$banner = $produtoService->getProdutos(null, 'banner');
 
 view('index', [
     'bestSellers' => $bestSellers,
