77a29ec007dfa8488ecedac5ed4bf187c8e266c7
Burnninel <brunoabc11@gmail.com>
2025-03-27 21:25:18 -0300

    Refatoração do 'categoria.controller' para consumo do service de produto
    A controller de categoria foi refatorada para utilizar o service de produto, centralizando a lógica de consulta ao banco de dados. Com essa alteração, o código ficou mais limpo, reutilizável e dinâmico, além de eliminar a repetição e melhorar a manutenção.

--- controllers/categoria.controller.php
+++ controllers/categoria.controller.php
@@ -3,26 +3,8 @@
 $categoria = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
 $filtroModelo = $_GET['modelo'] ?? 'todos';
 
-$produtos = (new DB())->query(
-    query: "
-        SELECT 
-            p.id AS id, p.nome AS nome, p.preco AS preco,
-            p.banner AS banner, p.best_seller AS bestSeller, p.lancamento AS lancamento,  
-            e.categoria AS categoria, e.modelo AS modelo,
-            (SELECT GROUP_CONCAT(caminho || '.' || tipo, ',') 
-            FROM imagens 
-            WHERE produto_id = p.id 
-            ORDER BY id) AS imagens
-        FROM produtos AS p
-        JOIN especificacao AS e ON e.id = p.especificacao_id
-        GROUP BY p.id
-    ",
-    class: $class = Produto::class,
-)->fetchAll();
-
-foreach ($produtos as $item) {
-    $item->setImagens($item->imagens);
-}
+$produtoService = new ProdutoServices();
+$produtos = $produtoService->getProdutos();
 
 $produtoCategoria = array_filter($produtos, function ($produto) use ($filtroModelo, $categoria) {
     return $produto->categoria === $categoria && 
