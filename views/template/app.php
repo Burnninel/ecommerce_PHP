8c3db66dce2f4715e8484c764ab51c0e40789adf
Burnninel <brunoabc11@gmail.com>
2025-03-28 23:46:38 -0300

    Criando o template 'header.php' e ajustando o 'app.php' com o require chamando o template do header
    Foi criado o template 'header.php' e ajustado o 'app.php' para incluir o header, utilizando o require para chamar o template de forma centralizada.

--- views/template/app.php
+++ views/template/app.php
@@ -13,46 +13,8 @@
 </head>
 
 <body class="bg-stone-100 tracking-wide">
-    <header>
-        <nav class="bg-zinc-900 text-center">
-            <div class="flex justify-between py-4 h-full items-center grid grid-cols-8 gap-4">
-                <a href="/" id="logo"
-                    class="text-5xl cursor-pointer uppercase text-emerald-900 col-span-4 col-start-3 hover:animate-pulse
-                ">shopninel</a>
-                <div class="bg-transparent border-b-2 flex px-2">
-                    <input type="text"
-                        class="h-xl bg-zinc-900 outline-none text-gray-200 text-lg 
-                            placeholder:text-gray-400 placeholder:italic"
-                        placeholder="Pesquisar...">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        class="size-6 text-gray-200"
-                        fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                    </svg>
-                </div>
-            </div>
-            <ul class="sm:text-sm text-[0.6rem] text-gray-950 bg-zinc-50 uppercase font-bold flex">
-                <li class="cursor-pointer py-2 w-1/4
-                           hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
-                    <a href="/camisetas" class="w-full h-full flex items-center justify-center">Camisetas</a>
-                </li>
-                <li class="cursor-pointer py-2 w-1/4
-                           hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
-                    <a href="/casacos" class="w-full h-full flex items-center justify-center">Casacos</a>
-                </li>
-                <li class="cursor-pointer py-2 w-1/4
-                           hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
-                    <a href="/calcas" class="w-full h-full flex items-center justify-center">Calças</a>
-                </li>
-                <li class="cursor-pointer py-2 w-1/4
-                           hover:duration-500 hover:bg-zinc-800 hover:text-gray-200">
-                    <a href="/bermudas" class="w-full h-full flex items-center justify-center">Bermudas</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-
+    <?php require "views/template/header.php"; ?>
+    
     <main class="px-[4vh]">
         <?php require "views/{$view}.view.php"; ?>
     </main>
