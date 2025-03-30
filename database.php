bed2ffee0c7895135708132bf6ee9b1327c1e001
Burnninel <brunoabc11@gmail.com>
2025-03-30 19:55:33 -0300

    Refatorando classe DB para suporte a múltiplos drivers e melhor configuração da DSN
    Alterada a classe DB para aceitar um array de configuração, permitindo maior flexibilidade no suporte a múltiplos bancos de dados. Agora, o DSN é gerado dinamicamente com base no driver informado. Além disso, foi adicionada a função lastInsertId() para recuperar o último ID inserido.

--- database.php
+++ database.php
@@ -4,15 +4,30 @@ class DB
 {
     private $db;
 
-    public function __construct() {
-        $this->db = new PDO('sqlite:database.sqlite');
+    public function __construct($config)
+    {
+        $this->db = new PDO($this->getDsn($config));
+    }
+
+    private function getDsn($config) {
+        
+        $driver = $config['driver'];
+        unset($config['driver']);
+
+        $dsn = $driver .':'. http_build_query($config, '', ';');
+
+        if($driver === 'sqlite') {
+            $dsn = $driver . ':' . $config['database'];
+        }
+
+        return $dsn;
     }
 
     public function query($query, $class = null, $params = [])
     {
         $prepare =  $this->db->prepare($query);
-        
-        if($class) {
+
+        if ($class) {
             $prepare->setFetchMode(PDO::FETCH_CLASS, $class);
         }
 
@@ -20,4 +35,10 @@ class DB
 
         return $prepare;
     }
+
+    public function lastInsertId() {
+        return $this->db->lastInsertId();
+    }
 }
+
+$database = new DB($config['database']);
