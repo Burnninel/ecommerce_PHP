0c27446e8cab54932385a93438b6a1d77ac3319e
Burnninel <brunoabc11@gmail.com>
2025-03-30 21:06:25 -0300

    Ajustando produto para ser indisponível sem medidas
    Este ajuste foi feito para garantir que, caso não existam dados correspondentes na tabela de medidas para um produto, o sistema marque o produto como indisponível, melhorando a experiência do usuário e evitando exibição de dados incorretos.

--- views/produto.view.php
+++ views/produto.view.php
@@ -16,12 +16,21 @@
     <div class="flex flex-col lg:w-1/3 mt-[8vh] px-[4vh]">
         <h2 class="text-md font-semibold"><?= $produto->nome ?></h2>
         <span class="text-md">R$ <?= $produto->preco ?></span>
-        <ul class="flex justify-between py-[4vh] uppercase text-sm">
-            <?php foreach ($dimensoes as $tamanhos): ?>
-                <li tabindex="0" class="border rounded-lg px-2 cursor-pointer" id="<?= $tamanhos->tamanho ?>"><?= $tamanhos->tamanho ?></li>
-            <?php endforeach; ?>
-        </ul>
-        <button class="w-full border-2 border-gray-900 rounded-xl border-gray-900 p-1 hover:bg-gray-900 hover:text-gray-50 font-medium uppercase">comprar</button>
+        <?php if (!empty($dimensoes)): ?>
+            <ul class="flex justify-between py-[4vh] uppercase text-sm">
+                <?php foreach ($dimensoes as $tamanhos): ?>
+                    <li tabindex="0" class="border rounded-lg px-2 cursor-pointer" id="<?= $tamanhos->tamanho ?>"><?= $tamanhos->tamanho ?></li>
+                <?php endforeach; ?>
+            </ul>
+            <button class="w-full font-medium uppercase border-2 border-gray-900 rounded-xl p-1 hover:bg-gray-900 hover:text-gray-50">
+                comprar
+            </button>
+        <?php else: ?>
+            <span class="uppercase text-[0.8rem] py-[4vh]">Produto indisponível</span>
+            <button class="w-full border-2 border-gray-500 text-gray-500 rounded-xl p-1 font-medium uppercase cursor-no-drop" disabled>
+                comprar
+            </button>
+        <?php endif; ?>
         <div class="flex flex-col pt-[4vh]">
             <div class="text-sm"><?= $produto->descricao ?></div>
             <p class="uppercase pt-4"><?= $produto->fabricacao ?></p>
@@ -33,27 +42,30 @@
         </ul>
 
         <div class="my-6">
-            <table id="medidasProduto" class="hidden w-full uppercase text-[0.6rem]">
-                <thead>
-                    <tr class="text-sm">
-                        <th class="p-1 text-left"></th> 
-                        <?php foreach ($dimensoes as $tamanho): ?>
-                            <th class="p-1 text-center"><?= $tamanho->tamanho ?></th>
-                        <?php endforeach; ?>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                     foreach (array_slice($medidas, 2) as $linha): ?>
-                        <tr>
-                            <td class="border border-black p-1 text-left"><?= ucfirst($linha) ?></td>
-                            <?php foreach ($dimensoes as $coluna): ?>
-                                <td class="border border-black p-1 text-left"><?= $coluna->{$linha} ?></td>
+            <?php if (!empty($dimensoes)): ?>
+                <table id="medidasProduto" class="w-full uppercase text-[0.6rem] hidden">
+                    <thead>
+                        <tr class="text-sm">
+                            <th class="p-1 text-left"></th>
+                            <?php foreach ($dimensoes as $tamanho): ?>
+                                <th class="p-1 text-center"><?= $tamanho->tamanho ?></th>
                             <?php endforeach; ?>
                         </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php foreach (array_slice($medidas, 2) as $linha): ?>
+                            <tr>
+                                <td class="border border-black p-1 text-left"><?= ucfirst($linha) ?></td>
+                                <?php foreach ($dimensoes as $coluna): ?>
+                                    <td class="border border-black p-1 text-left"><?= $coluna->{$linha} ?></td>
+                                <?php endforeach; ?>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            <?php else: ?>
+                <span id="medidasProduto" class="hidden uppercase text-[0.8rem] my-4">Dados indisponíveis</span>
+            <?php endif; ?>
 
             <div id="composicaoPorduto" class="hidden space-y-1 text-sm">
                 <p><?= $produto->composicao ?></p>
