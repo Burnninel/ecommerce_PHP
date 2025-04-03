93733f74284779b6fae0a38b11aa268d18e2f6f0
Burnninel <brunoabc11@gmail.com>
2025-04-02 21:20:21 -0300

    Ajustando exibição de imagem alternativa para produtos
    Substituído operador ternário por operador de coalescência nula (??) para garantir que, caso não haja imagem disponível, um fallback seja exibido.

--- views/template/cardProduto.php
+++ views/template/cardProduto.php
@@ -1,7 +1,12 @@
 <a href="produto?id=<?= $produto->id ?>" class="w-full">
     <div class="relative h-[45vw] md:h-[30vw] lg:h-[25vw]">
-        <img src="/img/<?= $produto->categoria ?>/<?= $produto->imagens[0] ?>" alt="Imagem da frente" class="absolute inset-0 w-full h-full transition-opacity duration-500 hover:opacity-0">
-        <img src="/img/<?= $produto->categoria ?>/<?= $produto->imagens[4] ? $produto->imagens[4] : $produto->imagens[3]?>" alt="Imagem de costas" class="absolute inset-0 w-full h-full transition-opacity duration-500 opacity-0 hover:opacity-100">
+        <img src="/img/<?= $produto->categoria ?>/<?= $produto->imagens[0] ?>"
+            alt="Imagem da frente"
+            class="absolute inset-0 w-full h-full transition-opacity duration-500 hover:opacity-0">
+
+        <img src="/img/<?= $produto->categoria ?>/<?= $produto->imagens[4] ?? $produto->imagens[4] ?? $produto->imagens[3] ?? 'fallback.png' ?>"
+            alt="Imagem de costas"
+            class="absolute inset-0 w-full h-full transition-opacity duration-500 opacity-0 hover:opacity-100">
     </div>
     <div class="flex flex-col sm:flex-row justify-between uppercase sm:text-[1.4vh] text-[1vh] text-zinc-800 py-[0.5vh]">
         <h2 class="font-bold"><?= $produto->nome ?></h2>
